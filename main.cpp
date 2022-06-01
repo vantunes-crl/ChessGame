92beb75efdb2b724cceedca51b548efcacd29031
vantunes <valerio.asj@gmail.com>
2022-06-01 17:26:55 +0100

    done table logic

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
 #include "Ichess_pieces.hpp"
 #include "table.hpp"
 #include <memory>
+#include "pawn.hpp"
+#include "bishop.hpp"
+#include "horse.hpp"
+#include "queen.hpp"
+#include "rook.hpp"
+#include "king.hpp"
 
 int main()
 {
     table<Ichess_pieces *> Table;
 
-    Table.printTable();
+    Table[0] = {new rook, new horse, new bishop, new queen, new king, new bishop, new horse, new rook};
+    Table[1] = {new pawn, new pawn, new pawn, new pawn, new pawn, new pawn, new pawn, new pawn};
+    Table[2] = {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
+    Table[3] = {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
+    Table[4] = {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
+    Table[5] = {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
+    Table[6] = {new rook, new horse, new bishop, new queen, new king, new bishop, new horse, new rook};
+    Table[7] = {new pawn, new pawn, new pawn, new pawn, new pawn, new pawn, new pawn, new pawn};
 
+
+    Table[2][1] = std::move(Table[1][0]);
+    Table[1][0] = nullptr;
+
+
+
+    Table.printTable();
 
 }
--- table.hpp
+++ table.hpp
@@ -1,15 +1,15 @@
 #ifndef TABLE_HPP
 #define TABLE_HPP
 
-#include "pawn.hpp"
 #include <array>
 #include <iostream>
+#include <iomanip>
 
 template <class T>
 class table
 {
 private:
-    std::array<std::array<T,8>, 8> PlayTable;
+    std::array<std::array<T, 8>, 8> PlayTable;
 public:
     void printTable()
     {
@@ -17,17 +17,29 @@ public:
         {
             for (int j = 0; j < 8; ++j )
             {
-                PlayTable[i][j] = new pawn;
-                PlayTable[i][j]->play();
+                std::cout << " " << std::setw(10);
+                if (PlayTable[i][j])
+                    std::cout << PlayTable[i][j]->type();
+                else
+                    std::cout << "null";
             }
             std::cout << std::endl;
         }
     }
 
-    std::array<int, 8> &operator[](const int i) { return PlayTable[i]; }
+    std::array<T, 8> &operator[](const int i) { return PlayTable[i]; }
 
-    table() {};
-    ~table() {};
+    ~table() 
+    {
+        for (auto pieces : PlayTable)
+        {
+            for (auto piece : pieces)
+            {
+                if (piece)
+                    delete piece;
+            }
+        }
+    };
 
 };
 
