f061c6b129cf76b771efd632da03ff10c32cae42
vantunes <valerio.asj@gmail.com>
2022-06-03 15:03:25 +0100

    delete utils

--- main.cpp
+++ main.cpp
@@ -22,31 +22,31 @@ int main()
     Table[7] = {new rook(1), new horse(1),new bishop(1),new queen(1),new king(1),new bishop(1),new horse(1),new rook(1)};
 
 
-    //Table[6][0]->play(Table, 5, 0); //case try go back
-   // Table[5][0]->play(Table, 6, 0);
-    //Table[6][0]->play(Table, 5, 1);
+//     Table[6][0]->play(Table, 5, 0); //case try go back
+//    Table[5][0]->play(Table, 6, 0);
+//     Table[6][0]->play(Table, 5, 1);
     
-    //Table[1][0]->play(Table, -1, 1); //case out of table
-    //Table[1][0]->play(Table, 1, 0); //case same position
-    //Table[1][0]->play(Table, 2, 1); // case if can kill
-    //Table[1][0]->play(Table, 2, 0); //he can move
-    //Table[1][0]->play(Table, 3, 0); //try move more than 1 slot
-    // { //case of someone is in front
-    //     Table[1][0]->play(Table, 2, 0);
-    //     Table[2][0]->play(Table, 3, 0);
-    //     Table[3][0]->play(Table, 4, 0);
-    //     Table[4][0]->play(Table, 5, 0);
-    //     Table[5][0]->play(Table, 6, 0);
-    // }
+//     Table[1][0]->play(Table, -1, 1); //case out of table
+//     Table[1][0]->play(Table, 1, 0); //case same position
+//     Table[1][0]->play(Table, 2, 1); // case if can kill
+//     Table[1][0]->play(Table, 2, 0); //he can move
+//     Table[1][0]->play(Table, 3, 0); //try move more than 1 slot
+//     { //case of someone is in front
+//         Table[1][0]->play(Table, 2, 0);
+//         Table[2][0]->play(Table, 3, 0);
+//         Table[3][0]->play(Table, 4, 0);
+//         Table[4][0]->play(Table, 5, 0);
+//         Table[5][0]->play(Table, 6, 0);
+//     }
 
-    // Table[2][0] = std::move(Table[0][0]); 
-    // Table[0][0] = nullptr;
+    Table[2][0] = std::move(Table[0][0]);
+    Table[0][0] = nullptr;
 
     // Table[2][4] = std::move(Table[1][4]); 
     // Table[1][4] = nullptr;
 
 
-    Table[0][0]->play(Table, 0, 0);
+    //Table[0][0]->play(Table, 0, 0);
 
 
     // Table[1][1]->play(Table, 2, 1); test if he can kill friend
