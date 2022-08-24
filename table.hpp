deaa546ac5a2c22974fa45be1a284126c94be4b3
valerio <valerio.asj@gmail.com>
2022-08-24 18:07:35 +0100

    Convert table to board name, and change matrix to array 64

--- table.hpp
+++ table.hpp
@@ -54,4 +54,46 @@ class table
 };
 
 
+template <class T>
+class Board 
+{
+    public:
+        void printTable()
+        {
+            std::array<std::string, 12> Pieces = {
+            "\033[1;39mBishop\033[0m", "\033[1;39mHorse\033[0m",
+            "\033[1;39mKing\033[0m", "\033[1;39mPawn\033[0m",
+            "\033[1;39mQueen\033[0m", "\033[1;39mRook\033[0m",
+            "\033[1;30mBishop\033[0m", "\033[1;30mHorse\033[0m",
+            "\033[1;30mKing\033[0m", "\033[1;30mPawn\033[0m",
+            "\033[1;30mQueen\033[0m", "\033[1;30mRook\033[0m"
+            };
+
+            for (int i = 0; i < 64; ++i )
+            {
+                
+                if (i % 8 == 0)
+                    std::cout << std::left << "\n--------------------------------------------------------------------------\n" << i / 8 << " |";
+                std::cout << std::left << std::setw(20) << std::setfill(' ');
+                if (PlayBoard[i])
+                    std::cout << Pieces[PlayBoard[i]->type()];
+                else
+                    std::cout << "\033[1;32mempty\033[0m";   
+            }
+            std::cout << std::left << "\n--------------------------------------------------------------------------\n";
+        }
+
+        Board operator=(const std::array<T, 64> &boardArray)
+        {
+            PlayBoard = boardArray;
+            return *this;
+        }
+
+        std::array<T, 64> &operator[](const int i) { return PlayBoard[i]; }
+
+    private:
+        std::array<T, 64> PlayBoard;
+};
+
+
 #endif
